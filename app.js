35cc10d0c6a7472702e4aeb8db565fc542093241
OLINYA-PAUL <olinyacpaul7849@gmail.com>
2025-01-16 13:53:05 +0100

    url added to cors

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const cors = require("cors");
 app.use(
   cors({
     origin: [
-      "https://eshop-frontend-app.vercel.app/",
+      "https://eshop-frontend-app-git-main-tech-masterys-projects.vercel.app"
+      "https://eshop-frontend-app.vercel.app",
     ],
     credentials: true,
   })
